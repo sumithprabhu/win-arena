3e239b5056839a4bc64da5d126f8af14859b259c
sumith prabhu <sumithprabhu07@gmail.com>
2025-03-09 17:05:34 +0530

    contract chnaged

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="w-full text-center bg-black text-white p-4 mt-8">
-        <p>&copy; 2025 Mochi Gaming. All rights reserved.</p>
+        <p>&copy; 2025 Win Arena. All rights reserved.</p>
       </footer>
     );
   };
